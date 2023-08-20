e65d3b6c57aa68f9bdb70ae01f3a95c878cd4431
Mohammad_Alhallaq <mohammad.alhallaq@gmail.com>
2023-08-20 22:50:54 +0300

    wip

---
+++ methods/methods.go
@@ -0,0 +1,16 @@
+package methods
+
+type Money uint
+type MoneyAmounts []Money
+
+func (m *MoneyAmounts) Add(amount Money) {
+	*m = append(*m, amount)
+}
+
+func (m *MoneyAmounts) Sum() Money {
+	var total Money
+	for i := 0; i < len(*m); i++ {
+		total += (*m)[i]
+	}
+	return total
+}
